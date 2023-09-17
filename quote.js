ff478c99731095c89a00c1e1553d648cac70860a
chiagozie pio <nduagubapio@gmail.com>
2023-09-17 20:01:57 +0100

    our little-generator project is completed

--- quote.js
+++ quote.js
@@ -3,12 +3,19 @@ const author = document.querySelector(".author");
 const nxtBtn = document.querySelector(".nxtBtn");
 const tweetBtn = document.querySelector(".tweet");
 
+
+
 const theQuotes = async ()=>{
     const url = await fetch("https://api.quotable.io/random");
     const reponse = await url.json();
-    console.log(reponse);
     quote.textContent = reponse.content;
     author.textContent = reponse.author
 }
 
 theQuotes();
+
+nxtBtn.addEventListener("click",theQuotes);
+
+tweetBtn.addEventListener("click", ()=>{
+    window.open("https://twitter.com/intent/tweet?text=" + quote.textContent +"---- by " + author.textContent, "Tweet Window", "width=600,height=300");
+})
